7b05b67572db2af8c85401cab092f9b7fec94785
FacuRamallo <framallo.ing@gmail.com>
2021-11-23 21:53:39 +0100

    db test 1

---
+++ src/Controller/LEController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use App\models\ListElement;
+
+class LEController {
+
+    public function index() {
+        $listElements = (new ListElement())->all();
+        require_once __DIR__ . '/../views/pages/indexList.php';
+        
+    }
+    public function create() {
+        require_once __DIR__ . '/../views/pages/create.php';
+    }
+
+
+}
